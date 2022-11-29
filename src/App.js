de604c4fc49071ddc81c3da6049d87190ce8bbab
Afzal Malik <afzalmalik006@gmail.com>
2022-11-30 00:17:20 +0500

    NewsApp code pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+export default class App extends Component {
+  //6921d821fb914d008851cf613846eda9 News API key
+  pageSize = 6;
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="general" badgeColor="primary" />} />
+            <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="business" badgeColor="secondary" />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" badgeColor="success" />} />
+            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="general" badgeColor="primary" />} />
+            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="health" badgeColor="danger" />} />
+            <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="science" badgeColor="warning" />} />
+            <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="sports" badgeColor="info" />} />
+            <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="technology" badgeColor="dark" />} />
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
